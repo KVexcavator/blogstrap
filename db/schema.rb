e34e24087cc1ed45d8adf96dd719ddf4a1c3e427
KVexcavator <str_token@mail.ru>
2018-12-01 11:59:59 +0600

    update model Pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_011000) do
+ActiveRecord::Schema.define(version: 2018_12_01_052422) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
