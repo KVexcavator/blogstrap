c3334741f41c4eab893f2d307641f5d26d9b88c9
KVexcavator <str_token@mail.ru>
2018-12-07 18:08:15 +0600

    add model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_220017) do
+ActiveRecord::Schema.define(version: 2018_12_07_120237) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string "image"
